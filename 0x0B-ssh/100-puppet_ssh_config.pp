f15da9df20ce71c208118d26583cf83381658ca9
PhilBoa <philicbrefo98@gmail.com>
2023-08-27 16:06:26 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,7 +5,7 @@ file { '/etc/ssh/ssh_config':
 	ensure => present,
 }
 
-file_line { 'Turn off password auth':
+file_line { 'Turn off passwd auth':
 path	=> '/etc/ssh/ssh_config',
 line	=> 'PasswordAuthentication no',
 match	=> '^#PasswordAuthentication',
@@ -13,5 +13,5 @@ match	=> '^#PasswordAuthentication',
 
 file_line { 'Declare identity file':
 path	=> '/etc/ssh/ssh_config',
-line	=> 'IdentityFile ~/ .ssh/school',
+line	=> 'IdentityFile ~/.ssh/school',
 match	=> '^#IdentityFile',	
