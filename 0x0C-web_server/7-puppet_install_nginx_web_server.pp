3c253063fa233cf3a5bc5d2537fba45d0c639b18
PhilBoa <philicbrefo98@gmail.com>
2023-09-04 18:50:44 -0700

    Updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,54 +1,21 @@
-# installs and configures nginx
+# This Puppet manifest configures Nginx and performs a 301 redirection
 
-package {'nginx':
-    ensure => 'present',
-}
+  package { 'nginx':
+    ensure => installed,
+  }
 
+  file { '/var/www/html/nginx':
+    ensure => 'directory',
+  }
 
-file {'index":
-    ensure  => 'file',
-    path    => '/var/www/html/index.html',
-    content => 'Hello World!'
-}
+  exec { 'display_page':
+    command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/nginx/index.nginx-debian.html',
+  }
 
-file {'404":
-    ensure  => 'file',
-    path    => '/var/www/html/404.html',
-    content => 'Ceci n\'est pas une page\n'
-}
+  exec { 'redirect_config':
+    command => '/usr/bin/sudo /bin/echo "location /redirect_me { return 301 http://innovateweb.tech; }" >> /etc/nginx/sites-available/default',
+  }
 
-file {'default":
-    ensure  => 'file',
-    path    => '/etc/nginx/sites-enabled/default',
-    content => '
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-
-        location / {
-                try_files \$uri \$uri/ =404;
-        }
-
-        error_page 404 /404.html;
-        location = /404.html {
-                root /var/www/html;
-                internal;
-        }
-}',
-    notify  => Service['nginx']
-}
-
-service {'nginx':
-    ensure  => 'running',
-    enable  => 'true',
-    require => File['default']
-}                                                               
+  exec { 'server_start':
+    command => '/usr/bin/sudo /usr/sbin/service nginx start',
+  }                                                              
